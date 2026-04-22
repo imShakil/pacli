4cc18360de1cf462759e510e3f7615a14fcf8e6e
Mobarak Hosen <mobarak.hosen@mindstaq.com>
2026-04-22 22:50:46 +0600

    fix: import error in pacli init

--- pacli/__init__.py
+++ pacli/__init__.py
@@ -1,7 +1,11 @@
-from importlib.metadata import version, PackageNotFoundError, metadata
+from typing import Optional
+from importlib.metadata import version, PackageNotFoundError, metadata, PackageMetadata
+
+__metadata__: Optional[PackageMetadata]
 
 try:
     __version__ = version("pacli-tool")
     __metadata__ = metadata("pacli-tool")
 except PackageNotFoundError:
     __version__ = "Unknown"
+    __metadata__ = None
